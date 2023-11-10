372ccb3337675171f23a8ddb66a2e935cb6cbbbd
nicoseper89 <nicosepertino@hotmail.com>
2023-11-10 20:54:30 -0300

    ejercicio 1

---
+++ Unidad1/ejercicio1/ejercicio1.js
@@ -0,0 +1,8 @@
+// 1.Realice un algoritmo que muestre el nombre y apellido de un usuario
+
+function showFullName(name, surname) {
+  console.log("Nombre: " + name);
+  console.log("Apellido: " + surname);
+}
+
+showFullName("Carlos", "Garcia");
