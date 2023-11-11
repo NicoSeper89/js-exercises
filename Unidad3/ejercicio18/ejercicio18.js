7f0ef571cc761d38e74d55bcf955b848de085e3a
nicoseper89 <nicosepertino@hotmail.com>
2023-11-11 20:17:53 -0300

    ejercicio 18

---
+++ Unidad3/ejercicio18/ejercicio18.js
@@ -0,0 +1,67 @@
+/* 18. Se le solicita al usuario que ingrese los extremos de un rango numérico y un
+número. Realice un programa para informar si el número está dentro del rango.
+Si está en rango, informar si el número es par.
+Si no está dentro del rango, informar si es número es impar.
+Tenga en cuenta que el rango debe tener una diferencia mínima de 5 números
+enteros.
+*/
+
+function getTotal() {}
+
+const form = document.getElementById("form");
+const messege = document.getElementById("message");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const min = parseInt(document.getElementById("minInput").value);
+  const max = parseInt(document.getElementById("maxInput").value);
+  const number = parseInt(document.getElementById("numberInput").value);
+
+  let response = "";
+  let colorBackground = "";
+
+  if (max - min >= 5) {
+    if (min <= number && number <= max) {
+      if (number % 2 === 0) {
+        response = "El número está dentro de rango y es par";
+        colorBackground = "success";
+      } else {
+        response = "El número está dentro de rango pero no es par";
+        colorBackground = "warning";
+      }
+    } else {
+      if (number % 2 != 0) {
+        response = "El número está fuera de rango y es impar";
+        colorBackground = "success";
+      } else {
+        response = "El número está fuera de rango pero no es impar";
+        colorBackground = "warning";
+      }
+    }
+  } else {
+    response = "El rango debe ser mayor o igual a 5";
+    colorBackground = "danger";
+  }
+
+  messege.innerHTML =
+    "<span>Rango: " +
+    min +
+    " - " +
+    max +
+    "<br/>Número: " +
+    number +
+    "<br />Resultado: " +
+    response +
+    "</span>";
+  messege.className = "alert alert-" + colorBackground + " mt-3";
+
+  document.getElementById("minInput").value = "";
+  document.getElementById("maxInput").value = "";
+  document.getElementById("numberInput").value = "";
+});
+
+form.addEventListener("reset", function (e) {
+  message.innerHTML = "<span> Esperando dato. </span>";
+  message.className = "alert alert-secondary mt-3";
+});
