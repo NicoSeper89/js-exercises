7ceaa48e43ee8e696b1be7f7ce044aa548c32fb5
nicoseper89 <nicosepertino@hotmail.com>
2023-11-10 21:11:55 -0300

    ejercicio 2

--- Unidad1/ejercicio1.js
+++ Unidad1/ejercicio1.js
@@ -1,8 +1,6 @@
 // 1.Realice un algoritmo que muestre el nombre y apellido de un usuario
 
-function showFullName(name, surname) {
+export function showFullName(name, surname) {
   console.log("Nombre: " + name);
   console.log("Apellido: " + surname);
 }
-
-showFullName("Carlos", "Garcia");
---
+++ Unidad1/ejercicio2.js
@@ -0,0 +1,8 @@
+// 2. Realice un algoritmo que muestre el promedio de 3 notas
+
+export function getAverage(n1, n2, n3) {
+  average = (n1 + n2 + n3) / 3;
+  console.log("Notas: " + n1 + " " + n2 + " " + n3);
+  console.log("Average: " + average);
+}
+
