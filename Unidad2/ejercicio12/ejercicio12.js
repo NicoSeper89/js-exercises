1bf64458b2de463ba5dc2811c9d0b094b6e4963d
nicoseper89 <nicosepertino@hotmail.com>
2023-11-11 16:00:11 -0300

    ejercicio 12

---
+++ Unidad2/ejercicio12/ejercicio12.js
@@ -0,0 +1,26 @@
+/* 2. Realizar un sistema para el cálculo de sueldos de una empresa. Se le solicita que
+ingrese el valor de la hora y la cantidad de horas trabajadas en el mes. Realice un
+programa para calcular e informar el sueldo mensual del empleado.*/
+
+function getTotal() {}
+
+const form = document.getElementById("form");
+const messege = document.getElementById("message");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const valueByHour = document.getElementById("hourValueInput");
+  const hours = document.getElementById("hoursNumberInput");
+
+  messege.innerHTML = "El sueldo a pagar es: $" + valueByHour.value * hours.value;
+  messege.className = "alert alert-success mt-3";
+
+  valueByHour.value = "";
+  hours.value = "";
+});
+
+form.addEventListener("reset", function (e) {
+  message.innerHTML = "<span> Esperando datos. </span>";
+  message.className = "alert alert-secondary mt-3";
+});
