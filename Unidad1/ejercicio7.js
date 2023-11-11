24dee6f98cb9b1feeefd7b79309212ba3f35a5b2
nicoseper89 <nicosepertino@hotmail.com>
2023-11-10 22:08:14 -0300

    ejercicio 7

---
+++ Unidad1/ejercicio7.js
@@ -0,0 +1,6 @@
+// 7. Realice el algoritmo que arme y muestre el mensaje "Bienvenido", seguido del nombre y apellido del usuario.
+
+function printWelcomeMessage(name, surname) {
+  console.log("Bienvenido " + name + " " + surname);
+}
+
