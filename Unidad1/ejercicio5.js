218b39298474fb61b4735aa106b21e50da3770bc
nicoseper89 <nicosepertino@hotmail.com>
2023-11-10 21:43:00 -0300

    ejercicio 5

---
+++ Unidad1/ejercicio5.js
@@ -0,0 +1,8 @@
+// 5. Realice el algoritmo para calcular e informar el perímetro y el área del rectángulo.
+
+function getInformationRectangle(width, length){
+    console.log("Longitud: " + length);
+    console.log("Ancho: " + width);
+    console.log("Perímetro: " + ((width + length) * 2));
+    console.log("Área: " + (width * length));
+}
